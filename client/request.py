885127b308346da1c6dcab96fad692277b49ff43
whwndgus001 <whwndgus0000@naver.com>
2020-09-02 17:05:13 +0900

    web client 연습

---
+++ client/request.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+
+# GET
+
+
+http_response = urlopen('https://movie.naver.com/movie/sdb/rank/rmovie.nhn')
+body = http_response.read()
+html = body.decode('cp949')
+
+
+
+
+
+
+
+print(html)
