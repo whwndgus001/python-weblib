ee26d781e20fb48b6a7e4d5296201eeff0f03d72
whwndgus001 <whwndgus0000@naver.com>
2020-09-03 09:52:44 +0900

    web client 연습 #2

--- client/request.py
+++ client/request.py
@@ -1,16 +1,25 @@
-from urllib.request import urlopen
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
 
 # GET
 
 
-http_response = urlopen('https://movie.naver.com/movie/sdb/rank/rmovie.nhn')
+http_response = urlopen('https://www.example.com')
 body = http_response.read()
-html = body.decode('cp949')
+html = body.decode('utf-8')
+print(html)
 
+# POST
+data = {
+    'id' : 'kickscar',
+    'pw' : '1234'
+}
+data = urlencode(data).encode('utf-8')
 
+request = Request('https://www.example.com', data)
+request.add_header('Content-Type', 'text/html')
 
-
-
-
-
+http_response = urlopen(request)
+body = http_response.read()
+html = body.decode('utf-8')
 print(html)
